8d09d9aafdb816eab0a5f8767de537b25524c718
Brandon <brandon5.9@hotmail.com>
2024-07-23 16:59:54 -0400

    Calendar has been added and it render when the buttom is clicked

---
+++ client/src/sideBar/buttoms/TimeClock.js
@@ -0,0 +1,40 @@
+// TimeClockButton.js
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+const TimeClock = () => {
+    
+  const location = useLocation();
+  const [mapLoaded, setMapLoaded] = useState(false);
+
+  useEffect(() => {
+    if (location.pathname === '/timeclock' && !mapLoaded) {
+      initMap();
+    }
+  }, [location, mapLoaded]);
+
+  async function initMap() {
+    const { Map } = await google.maps.importLibrary("maps");
+
+    const map = new Map(document.getElementById("map"), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    });
+    setMapLoaded(true);
+  }
+
+  return (
+    <>
+      <li>
+        <Link to="/timeclock" className="side-buttons">
+          TimeClock
+        </Link>
+      </li>
+      {location.pathname === '/timeclock' && (
+        <div id="map" style={{ height: '500px', width: '100%' }}></div>
+      )}
+    </>
+  );
+};
+
+export default TimeClock;
