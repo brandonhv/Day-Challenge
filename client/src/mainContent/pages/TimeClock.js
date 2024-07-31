d6a6f0cad5176ba838bf5e9108677cfdf69db1bd
Brandon <brandon5.9@hotmail.com>
2024-07-31 14:27:01 -0400

    No main changes done

--- client/src/mainContent/pages/TimeClock.js
+++ client/src/mainContent/pages/TimeClock.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef } from "react";
 import { Loader } from "@googlemaps/js-api-loader";
 
-
 const TimeClock = () => {
-  const mapRef = useRef(null);
+
+  const mapRef = useRef(null); // Reference to the DOM element for the map
+  
 
   useEffect(() => {
     const loader = new Loader({
@@ -13,12 +14,14 @@ const TimeClock = () => {
 
     const initializeMap = async () => {
       try {
-        const google = await loader.load(); // Correctly load the Google Maps API
+        const google = await loader.load();
 
-        const map = new google.maps.Map(mapRef.current, {
-          center: { lat: 40.87420997197665, lng: -74.04980768292904 },   
+        let map = new google.maps.Map(mapRef.current, {
+          center: { lat: 40.87420997197665, lng: -74.04980768292904 },
           zoom: 11,
         });
+
+        console.log("This is map: ", map)
       } catch (error) {
         console.error("Error loading Google Maps:", error);
       }
@@ -27,16 +30,17 @@ const TimeClock = () => {
     initializeMap();
   }, []);
 
-
   return (
     <div>
       <h1>TimeClock</h1>
-      <div id="map" ref={mapRef} style={{ height: '500px', width: '100%' }}></div>
+      <div ref={mapRef} style={{ height: '680px', width: '100%' }}></div>
     </div>
   );
 };
 
 export default TimeClock;
+
+
 
 
 
